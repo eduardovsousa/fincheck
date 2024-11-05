5bced172861f2f8f389b3bd498c3858536b9c70d
eduardovsousa <eduardo.varela@outlook.com.br>
2024-11-05 13:15:36 -0300

    feat: create categories module

--- api/src/modules/auth/auth.service.ts
+++ api/src/modules/auth/auth.service.ts
@@ -5,7 +5,7 @@ import {
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { compare, hash } from 'bcryptjs';
-import { UsersRepository } from 'src/shared/repositories/users.repositories';
+import { UsersRepository } from 'src/shared/database/repositories/users.repositories';
 import { SigninDto } from './dto/signin.dto';
 import { SignupDto } from './dto/signup.dto';
 
