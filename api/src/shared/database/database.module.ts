08a7466c5138c20357b472af0dbc8156db0d920d
eduardovsousa <eduardo.varela@outlook.com.br>
2024-11-05 14:24:46 -0300

    feat: create resource to bank accounts and validate if is the owner to make any change

--- api/src/shared/database/database.module.ts
+++ api/src/shared/database/database.module.ts
@@ -1,11 +1,17 @@
 import { Global, Module } from '@nestjs/common';
 import { UsersRepository } from '../database/repositories/users.repositories';
 import { PrismaService } from './prisma.service';
+import { BankAccountsRepository } from './repositories/bank-accounts.repositories';
 import { CategoriesRepository } from './repositories/categories.repositories';
 
 @Global()
 @Module({
-  providers: [PrismaService, UsersRepository, CategoriesRepository],
-  exports: [UsersRepository, CategoriesRepository],
+  providers: [
+    PrismaService,
+    UsersRepository,
+    CategoriesRepository,
+    BankAccountsRepository,
+  ],
+  exports: [UsersRepository, CategoriesRepository, BankAccountsRepository],
 })
 export class DatabaseModule {}
