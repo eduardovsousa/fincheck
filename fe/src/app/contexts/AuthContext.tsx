c712c60e18703a4ddca1bfe13005eaeda8701ab6
eduardovsousa <eduardo.varela@outlook.com.br>
2024-11-12 17:09:40 -0300

    feat: create sign out function

--- fe/src/app/contexts/AuthContext.tsx
+++ fe/src/app/contexts/AuthContext.tsx
@@ -4,6 +4,7 @@ import { localStorageKeys } from "../config/localStorageKeys";
 interface AuthContextProps {
   signedIn: boolean;
   signin(accessToken: string): void;
+  signout(): void;
 }
 
 // eslint-disable-next-line react-refresh/only-export-components
@@ -24,8 +25,14 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
     setSignedIn(true);
   }, []);
 
+  const signout = useCallback(() => {
+    localStorage.removeItem(localStorageKeys.ACCESS_TOKEN);
+
+    setSignedIn(false);
+  }, []);
+
   return (
-    <AuthContext.Provider value={{ signedIn, signin }}>
+    <AuthContext.Provider value={{ signedIn, signin, signout }}>
       {children}
     </AuthContext.Provider>
   );
--- fe/src/view/pages/Dashboard/index.tsx
+++ fe/src/view/pages/Dashboard/index.tsx
@@ -1,7 +1,13 @@
+import { useAuth } from "../../../app/hooks/useAuth";
+import { Button } from "../../components/Button";
+
 export function Dashboard() {
+  const { signout } = useAuth();
   return (
-    <h1>
+    <div>
       <span>Dashboard page</span>
-    </h1>
+
+      <Button onClick={signout}>Sair</Button>
+    </div>
   );
 }
