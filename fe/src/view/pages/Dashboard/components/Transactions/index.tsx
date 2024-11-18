4a04254adde268f7af47dda83145273563905404
eduardovsousa <eduardo.varela@outlook.com.br>
2024-11-18 11:33:28 -0300

    fix: transaction double refetch by swipper

--- fe/src/view/pages/Dashboard/components/Transactions/index.tsx
+++ fe/src/view/pages/Dashboard/components/Transactions/index.tsx
@@ -59,6 +59,7 @@ export function Transactions() {
                 centeredSlides
                 initialSlide={filters.month}
                 onSlideChange={(swiper) => {
+                  if (swiper.realIndex === filters.month) return;
                   handleChangeMonth(swiper.realIndex);
                 }}
               >
